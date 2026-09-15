629b02196731df7b38746d34d664be70535856d4
salt710 <yjyeom@gmail.com>
2026-09-15 10:35:29 +0900

    Lecture: Euclidean, GF element

---
+++ Lecture_Rec/NumberTheory/GF_256/GF_256.cpp
@@ -0,0 +1,74 @@
+﻿// GF_256.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+// GF(2^8)의 데이터, 연산의 구현
+
+//데이터 형: 바이트
+// byte b = b7b6b5b4 b3b2b1b0 =  b7*x^7 + b6*x^6 + ... + b1*x + b0
+// 예: 0x0a = 0000 1010 = x^3 + x
+typedef unsigned char byte; // 0~255 ==> 256가지의 다항식
+
+// 유한체 GF(2^8)의 덧셈
+// a = a7a6...a1a0 = a7*x^7 + a6*x^6 + ... + a1*x + a0
+// b = b7b6...b1b0 = b7*x^7 + b6*x^6 + ... + b1*x + b0
+// c = a + b
+// c = (a7+b7)*x^7 + (a6+b6)*x^6 + ... + (a1+b1)*x + (a0+b0)
+// 계수들은 0 또는 1 (GF(2)의 원소): 0+0=0, 0+1=1, 1+1=0
+// 비트 x, y의 xor :  x xor y = 1 (x,y 중 하나만 1), 0(x,y 모두 0 또는 모두 1)
+byte GF256_add(byte a, byte b) {
+    return a ^ b; // ^ : xor 연산자
+}
+
+// GF(2^8)의 원소를 비트열로 출력하기
+// b = b7b6...b1b0 ==> [b7,b6,...,b1,b0]
+void GF256_print_bin(byte b) {
+    byte b_coeff[8]; // 계수를 저장하는 배열
+    for (int i = 0; i < 8; i++) {
+        // 계수: b7, b6, ... , b0 순서로 추출하기
+        b_coeff[i] = (b >> (7 - i)) & 0x01;
+    }
+    for (int i = 0; i < 8; i++) {
+        printf("%d", b_coeff[i]);
+    }
+    printf("\n");
+}
+
+// GF(2^8)의 원소를 다항식으로 출력하기
+// b = b7b6...b1b0 ==> b7*x^7 + ... + b0
+void GF256_print_poly(byte b) {
+    byte b_coeff[8]; // 계수를 저장하는 배열
+    for (int i = 0; i < 8; i++) {
+        // 계수: b7, b6, ... , b0 순서로 추출하기
+        b_coeff[i] = (b >> (7 - i)) & 0x01; 
+    }
+
+    int coef, expo;
+    for (int i = 0; i < 8; i++) {
+        coef = b_coeff[i]; // b7, b6, ... , b0
+        expo = 7 - i; // 7, 6, ... , 1, 0
+        if (coef == 1) {
+            printf("+x^%1d ", expo);
+        }        
+    }
+    printf("\n");
+}
+
+int main()
+{
+    byte a, b, c;
+    a = 0x03;
+    b = 0x1a;
+    printf("a = ");
+    GF256_print_bin(a);
+    GF256_print_poly(a);
+    printf("b = ");
+    GF256_print_bin(b);
+    GF256_print_poly(b);
+    c = GF256_add(a, b);
+    printf("c = ");
+    GF256_print_bin(c);
+    GF256_print_poly(c);
+}
+
---
+++ Lecture_Rec/NumberTheory/exEnclidAlgorithm/exEnclidAlgorithm.cpp
@@ -0,0 +1,160 @@
+﻿// exEnclidAlgorithm.cpp : 
+//
+
+#include <stdio.h>
+
+//gcd를 구하는 함수
+// a = bq+r 이면, gcd(a,b) = gcd(b,r)
+// 반복하면서 b==0 일때, a가 gcd이다.
+//  a = bq + r
+//  b = rq'+r' ...
+int gcd(int a, int b) {
+    int r, q; // r:나머지, q: 몫
+    while (b != 0) {
+        r = a % b; // a를 b로 나눈 나머지
+        q = a / b; // a를 b로 나눈 몫
+
+        printf("%d = %d*%d + %d\n", a, b, q, r);//a=bq+r
+
+        a = b;
+        b = r;
+    }
+    return a; 
+}
+
+//확장 유클리드 알고리즘
+// gcd(a,b) = a*x + b*y 인 x,y도 gcd와 함께 구하기
+// 최초 입력 파라미터로 받은 a,b는 변하지 않게
+// new_a = ua*a + va*b  (a,b의 일차결합)
+// new_b = ub*a + vb*b  (a,b의 일차결합)
+int ex_gcd(int a, int b) {
+    int q; // q: 몫
+    int ua, va, ub, vb;
+    int n_ua, n_va, n_ub, n_vb;
+    int an, bn; // 계속 업데이트되는 a,b의 값을 an, bn이라 하자.
+    int new_an, new_bn;
+
+    an = a; bn = b;
+    ua = 1; va = 0; // an = ua*a + va*b (초깃값 = 1*a + 0*b)
+    ub = 0; vb = 1; // bn = ub*a + vb*b (초깃값 = 0*a + 1*b)
+
+    while (bn != 0) {
+        // an = bn*q+r 
+        // new_an = bn = ub*a + vb*b (n_ua = ub, n_va = vb)
+        // new_bn = r = an - bn*q = (ua*a + va*b) - (ub*a + vb*b)*q
+        //        = (ua - ub*q)*a + (va - vb*q)*b = n_ub*a + n_vb*b        
+        printf("%d=(%d)*%d+(%d)*%d,\t %d=(%d)*%d+(%d)*%d\n",
+            an, ua, a, va, b, bn, ub, a, vb, b);
+
+        q = an / bn; 
+        new_an = bn;
+        new_bn = an - bn * q; //an = bn*q+r 
+
+        n_ua = ub; n_va = vb;
+        n_ub = ua - ub * q; n_vb = va - vb * q;
+
+        an = new_an; bn = new_bn;
+        ua = n_ua; va = n_va; ub = n_ub; vb = n_vb;
+    }
+    return an;
+}
+
+//확장 유클리드 알고리즘(C++ 버전)
+// gcd(a,b) = a*x + b*y 인 x,y도 gcd와 함께 구하기 
+// x, y값도 함수 결과로 활용하기
+int ex_gcd_cpp(int a, int b, int &x, int &y) { // 
+    int q; // q: 몫
+    int ua, va, ub, vb;
+    int n_ua, n_va, n_ub, n_vb;
+    int an, bn; // 계속 업데이트되는 a,b의 값을 an, bn이라 하자.
+    int new_an, new_bn;
+
+    an = a; bn = b;
+    ua = 1; va = 0; // an = ua*a + va*b (초깃값 = 1*a + 0*b)
+    ub = 0; vb = 1; // bn = ub*a + vb*b (초깃값 = 0*a + 1*b)
+
+    while (bn != 0) {
+        // an = bn*q+r 
+        // new_an = bn = ub*a + vb*b (n_ua = ub, n_va = vb)
+        // new_bn = r = an - bn*q = (ua*a + va*b) - (ub*a + vb*b)*q
+        //        = (ua - ub*q)*a + (va - vb*q)*b = n_ub*a + n_vb*b        
+        printf("%d=(%d)*%d+(%d)*%d,\t %d=(%d)*%d+(%d)*%d\n",
+            an, ua, a, va, b, bn, ub, a, vb, b);
+
+        q = an / bn;
+        new_an = bn;
+        new_bn = an - bn * q; //an = bn*q+r 
+
+        n_ua = ub; n_va = vb;
+        n_ub = ua - ub * q; n_vb = va - vb * q;
+
+        an = new_an; bn = new_bn;
+        ua = n_ua; va = n_va; ub = n_ub; vb = n_vb;
+    }
+    x = ua; y = va; // an = ua*a + va*b
+    return an;
+}
+
+//재귀호출(recursion)을 이용하는 방법
+// 효율적이진 않지만 직관적으로 이해하기 쉬울수도...
+int gcd_recursive(int a, int b) {
+    // gcd(a,b) = gcd(b,r)이고, b==0이면 gcd=a
+    if (b == 0) {
+        return a;
+    }
+    else {
+        printf("call gcd_recursive(%d,%d)\n", b, a % b);
+        return gcd_recursive(b, a % b); // gcd(b,r)
+    }
+
+}
+
+//간단한 재귀호출 버전
+// 3항 연산자를 사용 ( result =  Q ? Y:N )
+int gcd_simple(int a, int b) {
+    return (b == 0) ? a : gcd_simple(b, a % b);
+}
+
+
+void gcd_test()
+{
+    int a, b, c, rc, sc;
+    a = 97;
+    b = 72;
+    c = gcd(a, b);
+    printf("a=%d, b=%d, gcd(%d,%d) = %d\n", a, b, a, b, c);
+
+    printf("GCD recursive version\n");
+    rc = gcd_recursive(a, b);
+    printf("a=%d, b=%d, gcd_recursive(%d,%d) = %d\n", a, b, a, b, rc);
+
+    printf("GCD simple version\n");
+    sc = gcd_simple(a, b);
+    printf("a=%d, b=%d, gcd_simple(%d,%d) = %d\n", a, b, a, b, sc);
+}
+
+
+void exgcd_test()
+{
+    int a, b, c, cpp_c;
+    a = 97;
+    b = 72;
+    c = ex_gcd(a, b);
+    printf("Extended Euclidean Algorithm\n");
+    printf("a=%d, b=%d, gcd(%d,%d) = %d\n", a, b, a, b, c);
+
+    printf("Extended Euclidean Algorithm (C++ version)\n");
+    int x, y;
+    cpp_c = ex_gcd_cpp(a, b, x, y);
+    printf("a=%d, b=%d, gcd(%d,%d) = %d\n", a, b, a, b, cpp_c);
+    printf("gcd(%d,%d) = (%d)*%d + (%d)*%d = %d\n", a, b, x, a, y, b, cpp_c);
+
+
+}
+
+int main() {
+    //gcd_test();
+    exgcd_test();
+
+    return 0;
+}
